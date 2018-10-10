4c61c9e54a267973d9f5054d8c7168c4e71830a0
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-10-10 21:44:14 +0100

    Updated package.json to remove playground.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const Mutations = {};
+const Mutations = {
+  async createItem(_, args, ctx, info) {
+    // TODO: Check if they are logged in
+
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, info)
+
+    return item;
+  }
+};
 
 module.exports = Mutations;
