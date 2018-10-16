a2d397695ac9d64f3ee5f26324e510c56d615de4
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-10-16 23:33:19 +0100

    Conditionally showing Nav items based on being signed in or not. Using refetchQueries to run me query straight after signin and signup mutations.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-
 const Mutations = {
   async createItem(_, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -57,6 +56,27 @@ const Mutations = {
       maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
     });
     // finally return user the the browser
+    return user;
+  },
+  async signin(_, { email, password }, ctx, info) {
+    // 1. check if there is a user with that email
+    const user = await ctx.db.query.user({ where: { email } });
+    if (!user) {
+      throw new Error(`No such user found for email ${email}`);
+    }
+    // 2. check if their password is correct
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new Error('Invalid password!');
+    }
+    // 3. generate the jwt token
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // 4. set the cookie with the token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+    // 5. return the user
     return user;
   }
 };
