c789017e3f57cbdbc03c41fa906d348dc03fcae9
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-10-18 00:30:01 +0100

    Reset password working on backend.

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -112,11 +112,35 @@ const Mutations = {
     }
     // 2. check if its a legit reset token
     // 3. check if its expired
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+      }
+    });
+    if (!user) {
+      throw new Error('This token is either invalid or expired!');
+    }
     // 4. hash their new password
+    const password = await bcrypt.hash(args.password, 10);
     // 5. save a new password to the user and remove old resetToken fields
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null
+      }
+    });
     // 6. generate jwt
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
     // 7. set the jwt cookie
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
     // 8. return the new user
+    return updatedUser;
 
   }
 };
