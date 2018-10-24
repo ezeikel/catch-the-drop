7238befe6defc21eb6b844fec1402037b70f2185
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-10-25 00:17:56 +0100

    Display cart items and totals.

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation addToCart($id: ID!) {
@@ -19,9 +20,11 @@ class AddToCart extends Component {
         mutation={ADD_TO_CART_MUTATION}
         variables={{
           id
-        }}>
-          {addToCart => (
-            <button onClick={addToCart}>Add To Cart 🛒</button>
+        }}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+        >
+          {(addToCart,  { loading }) => (
+            <button onClick={addToCart} disabled={loading}>Add{loading && 'ing'} To Cart 🛒</button>
           )}
       </Mutation>
     );
