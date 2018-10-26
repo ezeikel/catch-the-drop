7ba6c20060426f98516d045f6d8d6134aa02c49e
Ezeikel Pemberton <ezeikelpemberton@gmail.com>
2018-10-26 01:02:02 +0100

    Credit card processing with stripe - front end.

---
+++ frontend/components/TakeMyMoney.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { Mutation } from 'react-apollo';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import Error from './ErrorMessage';
+import User, { CURRENT_USER_QUERY } from './User';
+
+function totalItems(cart) {
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+}
+
+class TakeMyMoney extends Component {
+  onToken = res => {
+    console.log('onToken()');
+    console.log(res.id);
+  };
+
+  render() {
+    return (
+      <User>
+        {({ data: { me }}) => (
+          <StripeCheckout
+            amount={calcTotalPrice(me.cart)}
+            name="Catch the Drop"
+            description={`Order of ${totalItems(me.cart)} items`}
+            image={me.cart[0].item && me.cart[0].item.image}
+            stripeKey="pk_test_n51YxU9flEb8OuWR5RiGqDvi"
+            currency="USD"
+            email={me.email}
+            token={res => this.onToken(res)}
+          >
+            {this.props.children}
+          </StripeCheckout>
+        )}
+      </User>
+    );
+  }
+}
+
+export default TakeMyMoney;
